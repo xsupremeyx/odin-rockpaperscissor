40b37be510c9ddfaf72605fe85e7a0015931fe5d
Yash <86472828+xsupremeyx@users.noreply.github.com>
2025-07-25 07:01:49 +0000

    Complete the commandline game project! ver 1.0

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice(){
     let choice = Math.round(Math.random()*100)+1;
     if(choice <= 33) return "rock";
@@ -10,4 +12,47 @@ function getHumanChoice(){
     return choice;
 }
 
-getHumanChoice();
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        if(!((humanChoice=='rock')||(humanChoice=='paper')||(humanChoice=='scissor'))){
+            console.log("Wrong Player Input! Try Again next time!");
+            return 'invalid';
+        }
+        if ((humanChoice === 'rock' && computerChoice ==='scissor')||(humanChoice === 'scissor' && computerChoice ==='paper')||(humanChoice === 'paper' && computerChoice ==='rock')){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        }
+        else if (humanChoice === computerChoice){
+            console.log(`Draw! Both chose ${humanChoice}!`);
+        }
+        else{
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+        }
+        return 'success';
+    }
+    let rounds = 5;
+    for(let i = 0; i < rounds; i++){
+        let success = playRound(getHumanChoice(),getComputerChoice())
+        if (success==='invalid'){
+            rounds++;
+        }
+    }
+    if(humanScore > computerScore){
+        console.log(`You won the Game by ${humanScore-computerScore} points!`);
+    }
+    else if(humanScore < computerScore){
+        console.log(`You lost the Game by ${computerScore-humanScore} points!`);
+    }
+    else{
+        console.log('Game was a Draw!');
+    }
+    return;
+}
+
+playGame();
