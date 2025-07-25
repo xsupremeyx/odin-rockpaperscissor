8b1c84d21e1d40208869705b42f5aff736286074
Yash <86472828+xsupremeyx@users.noreply.github.com>
2025-07-25 03:55:45 +0000

    Complete getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+function getComputerChoice(){
+    let choice = Math.round(Math.random()*100)+1;
+    if(choice <= 33) return "rock";
+    else if(choice <= 66) return "paper";
+    else return "scissor";
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice: ");
+    return choice;
+}
+
+getHumanChoice();
